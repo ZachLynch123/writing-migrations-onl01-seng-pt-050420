e7b7202460d0afdceced3f337be53eaa8ed44c82
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-07-18 19:26:23 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,1 +1,9 @@
-class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.2]
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
+  
+  def change 
+    add_column :grade, :birthdate 
+  end
+  
+  
+  
+end
